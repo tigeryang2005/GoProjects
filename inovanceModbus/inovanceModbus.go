24335137f8a91b26762f85b2a123f3fb226ff9b6
yangtiger <yangtiger2005@163.com>
2025-05-12 19:04:41 +0800

    1.换了一种入库方式还是有时间戳一致问题2.库中时间戳与入库时间戳显示不一致，但用入库时间戳可以查到相应数据，应该是显示问题

--- inovanceModbus/inovanceModbus.go
+++ inovanceModbus/inovanceModbus.go
@@ -52,9 +52,9 @@ func ConnectInovanceModbus() {
 	quantity := uint16(125) // 读取数量
 
 	// totalReads := 100_000_000 // 1亿次读取
-	totaljobs := int64(0)   // 总读取次数 0表示无限读取
-	currentJobs := int64(0) // 已读取次数计数
-	errorCount := int32(0)  // 错误计数
+	totaljobs := int64(10_000) // 总读取次数 0表示无限读取
+	currentJobs := int64(0)    // 已读取次数计数
+	errorCount := int32(0)     // 错误计数
 
 	// 准备结果收集
 	resChanCount := 1000
@@ -65,30 +65,30 @@ func ConnectInovanceModbus() {
 	startTime := time.Now()
 
 	// 启动结果收集器
-	go func(writeAPI api.WriteAPI, startTime time.Time, currentJobs *int64) {
+	go func(writeAPI api.WriteAPI) {
 		defer close(done)
-		for res := range resChan {
-			// results = append(results, res)
-			for key, value := range res {
-				// 创建point
-				p := influxdb2.NewPointWithMeasurement("experiment").AddTag("location", "tianjin")
-				for count, v := range value {
-					p.AddField(fmt.Sprintf("sensor%d", count), v)
-				}
-				// todo:后面要把观察点删除，把入库放到worker里
-				atomic.AddInt64(currentJobs, 1)
-				p.AddField("num", *currentJobs)
-				// 写入数据到InfluxDB
-				p.SetTime(time.Unix(0, key.UnixNano()))
-				logger.Logger.Debug("写入数据", zap.Int64("时间戳", key.UnixNano()), zap.Any("num:", *currentJobs), zap.Any("值：", value))
-				writeAPI.WritePoint(p)
-				// 输出统计信息
-				// logger.Logger.Debug("写入完成", zap.Int64("总读取次数", *currentJobs))
-				if *currentJobs%10_000 == 0 {
-					resInfo(startTime, errorCount, *currentJobs)
-				}
-			}
-		}
+		// for res := range resChan {
+		// 	// results = append(results, res)
+		// 	for key, value := range res {
+		// 		// 创建point
+		// 		p := influxdb2.NewPointWithMeasurement("experiment").AddTag("location", "tianjin")
+		// 		for count, v := range value {
+		// 			p.AddField(fmt.Sprintf("sensor%d", count), v)
+		// 		}
+		// 		// todo:后面要把观察点删除，把入库放到worker里
+		// 		atomic.AddInt64(currentJobs, 1)
+		// 		p.AddField("num", *currentJobs)
+		// 		// 写入数据到InfluxDB
+		// 		p.SetTime(time.Unix(0, key.UnixNano()))
+		// 		// logger.Logger.Debug("写入数据", zap.Int64("时间戳", key.UnixNano()), zap.Any("num:", *currentJobs), zap.Any("值：", value))
+		// 		writeAPI.WritePoint(p)
+		// 		// 输出统计信息
+		// 		// logger.Logger.Debug("写入完成", zap.Int64("总读取次数", *currentJobs))
+		// 		if *currentJobs%10_000 == 0 {
+		// 			resInfo(startTime, errorCount, *currentJobs)
+		// 		}
+		// 	}
+		// }
 		// 确保所有数据都已写入
 		writeAPI.Flush()
 
@@ -99,10 +99,10 @@ func ConnectInovanceModbus() {
 				logger.Logger.Error("写入错误:", zap.Error(err))
 			}
 		}()
-	}(writeAPI, startTime, &currentJobs)
+	}(writeAPI)
 
 	jobs := make(chan struct{}, 1000)
-	interval := 100 * time.Microsecond // 读取间隔1微秒
+	interval := 1000 * time.Microsecond // 读取间隔1ms
 	// 启动周期性任务生成器
 	go continuousJobGenerator(interval, jobs, totaljobs)
 
@@ -110,7 +110,7 @@ func ConnectInovanceModbus() {
 	var wg sync.WaitGroup
 	for _, modbusClient := range modbusClients {
 		wg.Add(1)
-		go worker(modbusClient, address, quantity, jobs, resChan, &wg, &errorCount)
+		go worker(modbusClient, address, quantity, jobs, &wg, &errorCount, &currentJobs, writeAPI, startTime)
 	}
 
 	// 等待所有worker完成
@@ -122,7 +122,7 @@ func ConnectInovanceModbus() {
 
 }
 
-func worker(modbusClient modbus.Client, address, quantity uint16, jobs <-chan struct{}, resChan chan map[time.Time][]int16, wg *sync.WaitGroup, errorCount *int32) {
+func worker(modbusClient modbus.Client, address, quantity uint16, jobs <-chan struct{}, wg *sync.WaitGroup, errorCount *int32, currentJobs *int64, writeAPI api.WriteAPI, startTime time.Time) {
 	defer wg.Done()
 	for range jobs {
 		readRegistersTs := time.Now()
@@ -134,15 +134,46 @@ func worker(modbusClient modbus.Client, address, quantity uint16, jobs <-chan st
 		}
 		// Convert byte slice to int16 slice
 		int16Results := make([]int16, len(results)/2)
+		pMeasurement := "experiment"
+		pTags := map[string]string{"location": "tianjin"}
+		pFields := make(map[string]interface{})
+		pFields["num"] = *currentJobs
+		// p := influxdb2.NewPointWithMeasurement("experiment").AddTag("location", "tianjin")
 		for i := range int16Results {
 			rawValue := binary.BigEndian.Uint16(results[i*2:])
-			int16Results[i] = int16(rawValue)
+			// p.AddField(fmt.Sprintf("sensor%d", i), int16(rawValue))
+			pFields[fmt.Sprintf("sensor%d", i)] = int16(rawValue)
 		}
-		resultsMap := make(map[time.Time][]int16)
-		resultsMap[readRegistersTs] = int16Results
+		p := influxdb2.NewPoint(
+			pMeasurement,
+			pTags,
+			pFields,
+			readRegistersTs,
+		)
+		// p.AddField("num", *currentJobs)
+		// p.SetTime(time.Unix(0, readRegistersTs.UnixNano()))
+		// p.SetTime(readRegistersTs.UnixNano())
+		atomic.AddInt64(currentJobs, 1)
+		// 写入数据库
+		writeAPI.WritePoint(p)
+
+		if *currentJobs%10_000 == 0 {
+			resInfo(startTime, *errorCount, *currentJobs)
+		}
+		// 错误处理
+		errorsCh := writeAPI.Errors()
+		go func() {
+			for err := range errorsCh {
+				logger.Logger.Error("写入错误:", zap.Error(err))
+			}
+		}()
+
+		readFinishTs := time.Now().UnixNano()
+		// 计算读取时间
+		duration := readFinishTs - readRegistersTs.UnixNano()
 		// 记录读取到的数据
-		// logger.Logger.Debug("读取到数据", zap.Int64("时间戳", readRegistersTs.UnixNano()), zap.Any("数据", int16Results))
-		resChan <- resultsMap
+		logger.Logger.Debug("读取到数据", zap.Any("读取数量:", currentJobs), zap.Int64("时间戳", readRegistersTs.UnixNano()), zap.Any("读取时间(纳秒)", duration), zap.Any("数据", p.FieldList()))
+		// resChan <- resultsMap
 	}
 }
 
@@ -161,9 +192,19 @@ func continuousJobGenerator(interval time.Duration, jobs chan<- struct{}, totalJ
 			jobs <- struct{}{}
 		}
 	} else {
-		for range totalJobs {
+		// jobs <- struct{}{}
+		ticker := time.NewTicker(interval)
+		i := 0
+		defer ticker.Stop()
+		for range ticker.C {
 			jobs <- struct{}{}
+			i++
+			// logger.Logger.Debug("生成任务", zap.Any("时间戳", time.Now().UnixNano()), zap.Any("任务队列", len(jobs)), zap.Any("任务数量", i))
+			if int64(i) >= totalJobs {
+				break
+			}
 		}
+
 	}
 	defer close(jobs)
 }
